188baabc919b83fe585832b5aead41ecb71f5b12
Volodymyr Yalechko <Volodymyr.I.Yalechko@lpnu.ua>
2026-05-28 22:13:07 +0300

    Fix flake8 E501 line length in AuthorSerializer

--- author/serializers.py
+++ author/serializers.py
@@ -5,4 +5,11 @@ from author.models import Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ("id", "first_name", "last_name", "pseudonym", "age", "retired")
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "pseudonym",
+            "age",
+            "retired",
+        )
